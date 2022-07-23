71eea3f6632b3c809893df7b207e2fe3bb85053c
Rachel Conn <rconn478@gmail.com>
2022-07-23 13:46:32 -0500

    Ignore deprecation warnings from external libraries

--- App.tsx
+++ App.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import { LogBox } from 'react-native';
 import { Provider as PaperProvider } from 'react-native-paper';
 import { Provider as StoreProvider } from 'react-redux';
 import NavigationStack from './app/components/common/NavigationStack/NavigationStack';
 import store from './app/redux/store';
+
+// Ignore deprecation warnings from libraries - PropTypes aren't used anywhere in this codebase
+LogBox.ignoreLogs([
+  'ViewPropTypes will be removed',
+]);
 
 const App = () => {
   return (
