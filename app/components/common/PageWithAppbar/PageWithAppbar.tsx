caea801833fb71dd0e5cbf3377e8a114f7ba044b
Rachel Conn <rconn478@gmail.com>
2022-08-15 20:07:51 -0500

    PageWithAppbar changes - Add usePaper prop for pages that want a white background - Make content view scrollable

--- app/components/common/PageWithAppbar/PageWithAppbar.tsx
+++ app/components/common/PageWithAppbar/PageWithAppbar.tsx
@@ -1,6 +1,6 @@
 import { useNavigation } from '@react-navigation/native';
 import React from 'react';
-import { View } from 'react-native';
+import { ScrollView, View } from 'react-native';
 import { Appbar, useTheme } from 'react-native-paper';
 import FocusAwareStatusBar from '../FocusAwareStatusBar/FocusAwareStatusBar';
 import styles from './styles';
@@ -8,24 +8,28 @@ import styles from './styles';
 interface AppbarPageProps {
   children?: React.ReactNode,
   title: string,
+  usePaper?: boolean,
 };
 
-const PageWithAppbar: React.FC<AppbarPageProps> = ({ children, title }) => {
+const PageWithAppbar: React.FC<AppbarPageProps> = ({ children, title, usePaper = false }) => {
   const navigation = useNavigation();
   const theme = useTheme();
+
+  const backgroundComputedStyle = { ...styles.background };
+  if (usePaper) backgroundComputedStyle.backgroundColor = 'white';
 
   const handleBackPress = () => { navigation.goBack() };
 
   return (
-    <View style={styles.background}>
+    <View style={backgroundComputedStyle}>
       <FocusAwareStatusBar barStyle="light-content" translucent backgroundColor={`${theme.colors.primary}`} />
       <Appbar.Header>
         <Appbar.Action icon="arrow-left" onPress={handleBackPress} />
         <Appbar.Content title={title} />
       </Appbar.Header>
-      <View style={styles.contentArea}>
+      <ScrollView style={styles.contentArea}>
         {children}
-      </View>
+      </ScrollView>
     </View>
   );
 };
