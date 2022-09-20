695310e3056a2784bf0ca6ceb2f2f8a57f6b29a2
Rachel Conn <rconn478@gmail.com>
2022-09-19 21:23:26 -0500

    Improve styling and customization of <PageWithAppbar />

--- app/components/common/PageWithAppbar/PageWithAppbar.tsx
+++ app/components/common/PageWithAppbar/PageWithAppbar.tsx
@@ -1,17 +1,25 @@
 import { useNavigation } from '@react-navigation/native';
 import React from 'react';
-import { ScrollView, View } from 'react-native';
+import { ScrollView, StyleProp, View, ViewStyle } from 'react-native';
 import { Appbar, useTheme } from 'react-native-paper';
 import FocusAwareStatusBar from '../FocusAwareStatusBar/FocusAwareStatusBar';
 import styles from './styles';
 
-interface AppbarPageProps {
-  children?: React.ReactNode,
+interface PageWithAppbarProps {
   title: string,
+  children?: React.ReactNode,
+  scrollViewStyle?: any,
+  staticContent?: React.ReactNode,
   usePaper?: boolean,
 };
 
-const PageWithAppbar: React.FC<AppbarPageProps> = ({ children, title, usePaper = false }) => {
+const PageWithAppbar: React.FC<PageWithAppbarProps> = ({
+  title,
+  children,
+  scrollViewStyle = {},
+  staticContent,
+  usePaper = false,
+}) => {
   const navigation = useNavigation();
   const theme = useTheme();
 
@@ -20,16 +28,28 @@ const PageWithAppbar: React.FC<AppbarPageProps> = ({ children, title, usePaper =
 
   const handleBackPress = () => { navigation.goBack() };
 
+  const staticContentComponent = staticContent && (
+    <View style={styles.fixedContentContainer}>
+      {staticContent}
+    </View>
+  );
+
   return (
     <View style={backgroundComputedStyle}>
       <FocusAwareStatusBar barStyle="light-content" translucent backgroundColor={`${theme.colors.primary}`} />
-      <Appbar.Header>
-        <Appbar.Action icon="arrow-left" onPress={handleBackPress} />
-        <Appbar.Content title={title} />
-      </Appbar.Header>
-      <ScrollView style={styles.contentArea}>
-        {children}
-      </ScrollView>
+      <View style={styles.pageContainer}>
+        <Appbar.Header>
+          <Appbar.Action icon="arrow-left" onPress={handleBackPress} />
+          <Appbar.Content title={title} />
+        </Appbar.Header>
+        <ScrollView
+          style={styles.contentAreaContainer}
+          contentContainerStyle={{ ...styles.contentArea, ...scrollViewStyle }}
+        >
+          {children}
+        </ScrollView>
+        {staticContentComponent}
+      </View>
     </View>
   );
 };
--- app/components/common/PageWithAppbar/styles.ts
+++ app/components/common/PageWithAppbar/styles.ts
@@ -1,16 +1,30 @@
-import { Dimensions, StyleSheet } from "react-native"
+import { Dimensions, StatusBar, StyleSheet } from "react-native"
 
-export const windowSize = Dimensions.get('window');
+const windowSize = Dimensions.get('window');
+const statusBarHeight = StatusBar.currentHeight ?? 0;
+const pageHeight = windowSize.height - statusBarHeight
 
 export default StyleSheet.create({
   background: {
-    backgroundColor: 'lightgray',
     height: windowSize.height,
     width: windowSize.width,
+    backgroundColor: 'lightgray',
     display: 'flex',
     flexDirection: 'column',
   },
+  pageContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  contentAreaContainer: {
+    height: pageHeight - 56,
+  },
   contentArea: {
     padding: windowSize.width * 0.025,
+  },
+  fixedContentContainer: {
+    position: 'absolute',
+    right: 0,
+    bottom: 0,
   },
 });
