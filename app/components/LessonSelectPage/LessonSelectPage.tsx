a4b99a71421cea26e128797f2dd0a8447dea41a9
Rachel Conn <rconn478@gmail.com>
2022-10-13 16:54:11 -0500

    Display lesson completion status

--- app/components/LessonSelectPage/LessonSelectPage.tsx
+++ app/components/LessonSelectPage/LessonSelectPage.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import Lesson from '../../classes/lesson';
 import { setCurrentLesson } from '../../redux/currentLesson';
 import { useDispatch } from '../../redux/store';
+import { getLessonStatus, LessonStatus } from '../../utils/lessonStatus';
 import IconCard from '../common/IconCard/IconCard';
 import { LessonSelectPageNavigationProp } from '../common/NavigationStack/NavigationStack';
 import PageWithAppbar from '../common/PageWithAppbar/PageWithAppbar';
@@ -22,13 +23,39 @@ const lessons: Record<Lesson, LessonProperties> = {
 const LessonSelectPage: React.FC = () => {
   const dispatch = useDispatch();
   const navigation = useNavigation<LessonSelectPageNavigationProp>();
+  const [lessonStatus, setLessonStatus] = React.useState<Partial<Record<Lesson, LessonStatus>>>();
+
+  const fetchLessonStatus = () => {
+    getLessonStatus().then((fetchedLessonStatus) => {
+      setLessonStatus(fetchedLessonStatus);
+    });
+  };
+
+  // Query lesson status on initial render
+  React.useEffect(() => {
+    fetchLessonStatus();
+
+    // Force requery when refocused
+    const removeListener = navigation.addListener('focus', fetchLessonStatus);
+    return removeListener;
+  }, []);
+
+  // Don't render full page until lesson status is fetched
+  if (!lessonStatus) return <PageWithAppbar title="Lessons" />;
 
   const lessonCards = Object.entries(lessons).map(([title, props]) => {
+    const lesson = title as Lesson;
+
     const handlePress = () => {
-      const lesson = title as Lesson;
       dispatch(setCurrentLesson(lesson));
       navigation.navigate(lesson);
     };
+
+    const lessonCompleted = lessonStatus[lesson]?.completed;
+    const titleIconProps = lessonCompleted ? {
+      titleIcon: 'check-circle-outline',
+      titleIconColor: '#009412',
+    } : {};
 
     return (
       <IconCard
@@ -37,6 +64,7 @@ const LessonSelectPage: React.FC = () => {
         icon={props.icon}
         onPress={handlePress}
         key={title}
+        {...titleIconProps}
       />
     );
   });
